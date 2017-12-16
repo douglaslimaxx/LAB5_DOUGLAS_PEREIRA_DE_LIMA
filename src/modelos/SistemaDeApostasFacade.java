9677f9bb6689de560ee8a32ba83ca10b8f9572ad
Douglas Lima <douglaslima909@gmail.com>
2017-12-16 14:28:19 -0300

    Documentei a Facade do sistema

--- src/modelos/SistemaDeApostasFacade.java
+++ src/modelos/SistemaDeApostasFacade.java
@@ -2,14 +2,28 @@ package modelos;
 
 import easyaccept.EasyAccept;
 
+/**
+ * Representa uma Facade que controla o controller Sistema.
+ * @author Douglas Lima
+ *
+ */
 public class SistemaDeApostasFacade {
 
 	private Sistema sistema;
 	
+	/**
+	 * Método que inicializa um sistema a partir de um valor do caixa
+	 * inicial e um valor de taxa.
+	 * @param caixa itn que é o valor do caixa inicial do sistema
+	 * @param taxa double que é a taxa utilizada pelo sistema.
+	 */
 	public SistemaDeApostasFacade(int caixa, double taxa) {
 		this.sistema = new Sistema(caixa, taxa);
 	}
 	
+	/**
+	 * @see Sistema#getCaixa()
+	 */
 	public int recuperaValorCaixa() {
 		return this.sistema.getCaixa();
 	}
@@ -21,44 +35,73 @@ public class SistemaDeApostasFacade {
 		return this.sistema.cadastraCenario(descricao);
 	}
 	
+	/**
+	 * @see Sistema#exibirUmCenario(int)
+	 */
 	public String exibirUmCenario(int numeracao) {
 		return this.sistema.exibirUmCenario(numeracao);
 	}
-	
+
+	/**
+	 * @see Sistema#listaCenarios()
+	 */
 	public String exibirTodosCenarios() {
 		return this.sistema.listaCenarios();
 	}
-	
+
+	/**
+	 * @see Sistema#adicionaAposta(int, String, int, String)
+	 */
 	public void cadastrarAposta(int cenario, String apostador, int quantia, String previsao) {
 		this.sistema.adicionaAposta(cenario, apostador, quantia, previsao);
 	}
-	
+
+	/**
+	 * @see Sistema#valorTotalDeApostas(int)
+	 */
 	public int valorTotalDeApostas(int cenario) {
 		return this.sistema.valorTotalDeApostas(cenario);
 	}
-	
+
+	/**
+	 * @see Sistema#totalDeApostas(int)
+	 */
 	public int totalDeApostas(int cenario) {
 		return this.sistema.totalDeApostas(cenario);
 	}
-	
+
+	/**
+	 * @see Sistema#exibirApostasDeUmCenario(int)
+	 */
 	public String exibeApostas(int cenario) {
 		return this.sistema.exibirApostasDeUmCenario(cenario);
 	}
 	
+
+	/**
+	 * @see Sistema#encerrarCenario(int, boolean)
+	 */
 	public void fecharAposta(int cenario, boolean ocorreu) {
 		this.sistema.encerrarCenario(cenario, ocorreu);
 	}
-	
+
+	/**
+	 * @see Sistema#cenarioParaCaixa(int)
+	 */
 	public int getCaixaCenario(int cenario) {
 		return this.sistema.cenarioParaCaixa(cenario);
 	}
-	
+
+	/**
+	 * @see Sistema#retornaRateio(int)
+	 */
 	public int getTotalRateioCenario(int cenario) {
 		return this.sistema.retornaRateio(cenario);
 	}
 	
 	public static void main(String[] args) {
-		args = new String[] {"modelos.SistemaDeApostasFacade", "acceptance_test/us1_test.txt"};
+		args = new String[] {"modelos.SistemaDeApostasFacade", "acceptance_test/us1_test.txt",
+				"acceptance_test/us2_test.txt", "acceptance_test/us3_test.txt", "acceptance_test/us1_test.txt"};
 		EasyAccept.main(args);
 	}
 }
