15bb5a1827c7790b554673ca72975c558ef54dca
Douglas Lima <douglaslima909@gmail.com>
2018-01-29 21:41:26 -0300

    CORREÇÃO DE ERRO

--- src/modelos/Main.java
+++ src/modelos/Main.java
@@ -1,8 +1,8 @@
 package modelos;
 
-public class Main {
-	private static Main EasyAccept;
+import easyaccept.EasyAccept;
 
+public class Main {
 	public static void main(String[] args) {
 		args = new String[] { "modelos.Facade", "acceptance_test/us1_test.txt",
 				"acceptance_test/us2_test.txt", "acceptance_test/us3_test.txt", "acceptance_test/us4_test.txt" };
